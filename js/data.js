d40f8adfee05903ed419d6777aca036a1c26903a
Evgeniya-SS <54635160+Evgeniya-SS@users.noreply.github.com>
2019-09-28 16:51:43 +0300

    my-services-structure

--- js/data.js
+++ js/data.js
@@ -4,3 +4,37 @@ const achievements = [
     ['box', 245, 'Total projects'],
     ['box', 245, 'Total projects']
 ];
+
+
+const services = [
+    { 
+        icon: 'snowflake-o',
+        title: 'Search Optimization',
+        p: 'The9 is a graphically polished, interactive, easily customizable, highly modern, fast loading.',
+    },
+    { 
+        icon: 'snowflake-o',
+        title: 'Logo & Identify',
+        p: 'The9 is a graphically polished, interactive, easily customizable, highly modern, fast loading.',
+    },
+    { 
+        icon: 'snowflake-o',
+        title: 'Graphics Design',
+        p: 'The9 is a graphically polished, interactive, easily customizable, highly modern, fast loading.',
+    },
+    { 
+        icon: 'snowflake-o',
+        title: 'Fully Responsive',
+        p: 'The9 is a graphically polished, interactive, easily customizable, highly modern, fast loading.',
+    },
+    { 
+        icon: 'snowflake-o',
+        title: 'Advanced Options',
+        p: 'The9 is a graphically polished, interactive, easily customizable, highly modern, fast loading.',
+    },
+    { 
+        icon: 'snowflake-o',
+        title: 'Reasonable Pricing',
+        p: 'The9 is a graphically polished, interactive, easily customizable, highly modern, fast loading.',
+    },
+];
--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,30 @@
 function renderAchievements() {
     return;
 }
+
+function renderServices(target, data) {
+    let HTML = '';
+    let quantity = 0;
+
+    for (i=0; i<data.length; i++) {
+        const obj = data[i];
+
+        if (!obj.icon ||
+            !obj.title ||
+            !obj.p) {
+                continue;
+            }
+        if (quantity === 6) {
+            break;
+        }
+        if (obj.p) {
+            HTML += `<div class="block">
+                        <i class="fa fa-${obj.icon}"></i>
+                        <h4>${obj.title}</h4>
+                        <p>${obj.p}</p>
+                    </div>`
+            quantity++;
+        }
+    }
+    return document.getElementById(target).innerHTML = HTML;
+}
