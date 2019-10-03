f5798fa82e81a00b2677c4533c0298b13fcf3ae5
Renata <valanciuterenata@gmail.com>
2019-10-03 21:44:58 +0300

    testimonials data

--- js/data.js
+++ js/data.js
@@ -39,3 +39,28 @@ const services = [
         p: 'The9 is a graphically polished, interactive, easily customizable, highly modern, fast loading.',
     },
 ];
+
+//testimonials
+const testimonials = [
+    {
+        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
+        author: 'Nancy Bayers',
+        position: 'Co-founder'
+    },
+    {
+        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
+        author: 'Nancy Bayers',
+        position: 'Co-founder'
+    },
+    {
+        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
+        author: 'Nancy Bayers',
+        position: 'Co-founder'
+    },
+    {
+        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
+        author: 'Nancy Bayers',
+        position: 'Co-founder'
+    },
+];
+//testimonials end
